d97174955d09d03bfc3317a912f0b53e356d0725
giulpig <andrea.codutti20@gmail.com>
2021-12-24 13:04:45 +0100

    save before merge, work in progess

---
+++ ChessBoard.cpp
@@ -0,0 +1,30 @@
+#ifndef CHESSBOARD_CPP
+#define CHESSBOARD_CPP
+
+#include "ChessBoard.h"
+
+
+
+ChessBoard::ChessBoard(){
+    for(int i=0; i<SIZE; i++){
+        for(int j=0; j<SIZE; j++){
+            switch (initial_roles[i][j]){
+                //case 'R':
+                    //shared_ptr<ChessPiece> toAdd(new King());
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- ChessBoard.h
+++ ChessBoard.h
@@ -4,6 +4,14 @@
 #include "Enums.h"
 #include "Exceptions.h"
 #include "ChessPiece.h"
+#include "King.h"
+#include "Queen.h"
+#include "Bishop.h"
+#include "Knight.h"
+#include "Tower.h"
+#include "Pawn.h"
+#include "Dummy.h"
+
 
 #include <vector>
 #include <list>
@@ -15,11 +23,41 @@ using std::list;
 using std::shared_ptr;
 using std::pair;
 
+
+constexpr int SIZE = 8;
+
+constexpr char initial_roles[8][8] = {
+
+    {'T','C','A','D','R','A','C','T'},
+    {'P','P','P','P','P','P','P','P'},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' '},
+    {'P','P','P','P','P','P','P','P'},
+    {'T','C','A','D','R','A','C','T'}
+};
+
+
+constexpr bool intial_color[8][8] = {             //dummy chesspieces are black by convenction
+
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+};
+
+
+
 //add inclusion of other chessPieces
 class ChessBoard{
 
 private:
-    shared_ptr<ChessPiece> chessBoard[8][8];
+    shared_ptr<ChessPiece> chessBoard[SIZE][SIZE];
     list<shared_ptr<ChessPiece>> black, white;//le liste e la matrice devono puntare agli stessi oggetti, questa è la soluzione meno peggiore che mi è venuta in mente ma si può fare di meglio
 
 public:
