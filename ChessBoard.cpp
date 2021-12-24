9cf1d214cefae5ee1595571ca118c03189701fa7
giulpig <andrea.codutti20@gmail.com>
2021-12-25 00:19:42 +0100

    fi(r)st shitty fucking implementation of basic chessboard, MERRY CHRISTMAS

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -4,6 +4,40 @@
 #include "ChessBoard.h"
 
 
+void ChessBoard::addToPieceList(const shared_ptr<ChessPiece> piece, const side sid){
+    if(sid == side::black)
+        black.push_back(piece);
+    else
+        white.push_back(piece);
+}
+
+shared_ptr<ChessPiece> ChessBoard::copyPiece(const shared_ptr<ChessPiece> toCopy) const{
+    switch(toCopy->getRole()){
+        case role::king: {
+            return shared_ptr<ChessPiece>(new King(*toCopy));
+        }
+        case role::queen: {
+            return shared_ptr<ChessPiece>(new Queen(*toCopy));
+        }
+        case role::bishop: {
+            return shared_ptr<ChessPiece>(new Bishop(*toCopy));
+        }
+        case role::knight:{
+            return shared_ptr<ChessPiece>(new Knight(*toCopy));
+        }
+        case role::tower:{
+            return shared_ptr<ChessPiece>(new Tower(*toCopy));
+        }
+        case role::pawn:{
+            return shared_ptr<ChessPiece>(new Pawn(*toCopy));
+        }
+        case role::dummy:{
+            return shared_ptr<ChessPiece>(new Dummy());
+        }
+
+    }
+}
+
 ChessBoard::ChessBoard(){
     for(int i=0; i<SIZE; i++){
         for(int j=0; j<SIZE; j++){
@@ -12,55 +46,37 @@ ChessBoard::ChessBoard(){
                 case 'R': {
                     shared_ptr<ChessPiece> toAdd(new King(i, j, thisSide));
                     chessBoard[i][j] = toAdd;
-                    if(thisSide == side::black)
-                        black.push_back(toAdd);
-                    else
-                        white.push_back(toAdd);
+                    addToPieceList(toAdd, thisSide);
                     break;
                 }
                 case 'D': {
                     shared_ptr<ChessPiece> toAdd(new Queen(i, j, thisSide));
                     chessBoard[i][j] = toAdd;
-                    if(thisSide == side::black)
-                        black.push_back(toAdd);
-                    else
-                        white.push_back(toAdd);
+                    addToPieceList(toAdd, thisSide);
                     break;
                 }
                 case 'A': {
                     shared_ptr<ChessPiece> toAdd(new Bishop(i, j, thisSide));
                     chessBoard[i][j] = toAdd;
-                    if(thisSide == side::black)
-                        black.push_back(toAdd);
-                    else
-                        white.push_back(toAdd);
+                    addToPieceList(toAdd, thisSide);
                     break;
                 }
                 case 'C':{
                     shared_ptr<ChessPiece> toAdd(new Knight(i, j, thisSide));
                     chessBoard[i][j] = toAdd;
-                    if(thisSide == side::black)
-                        black.push_back(toAdd);
-                    else
-                        white.push_back(toAdd);
+                    addToPieceList(toAdd, thisSide);
                     break;
                 }
                 case 'T':{
                     shared_ptr<ChessPiece> toAdd(new Tower(i, j, thisSide));
                     chessBoard[i][j] = toAdd;
-                    if(thisSide == side::black)
-                        black.push_back(toAdd);
-                    else
-                        white.push_back(toAdd);
+                    addToPieceList(toAdd, thisSide);
                     break;
                 }
                 case 'P':{
                     shared_ptr<ChessPiece> toAdd(new Pawn(i, j, thisSide));
                     chessBoard[i][j] = toAdd;
-                    if(thisSide == side::black)
-                        black.push_back(toAdd);
-                    else
-                        white.push_back(toAdd);
+                    addToPieceList(toAdd, thisSide);
                     break;
                 }
                 case ' ':{
@@ -79,16 +95,58 @@ ChessBoard::ChessBoard(){
 
 ChessBoard::ChessBoard(const ChessBoard& o){
     for(int i=0; i<SIZE; i++){
-        
+        for(int j=0; j<SIZE; j++){
+            shared_ptr<ChessPiece> toAdd(copyPiece(o.chessBoard[i][j]));
+            chessBoard[i][j] = toAdd;
+        }
+    }
+    for(const auto &i: o.black){
+        black.push_back(i);
+    }
+    for(const auto &i: o.white){
+        white.push_back(i);
     }
 }
 
-ChessBoard::ChessBoard(const ChessBoard&&){
+ChessBoard::ChessBoard(ChessBoard&& o){
+    for(int i=0; i<SIZE; i++){
+        for(int j=0; j<SIZE; j++){
+            //chessBoard[i][j] = std::move(o.chessBoard[i][j]);
+            chessBoard[i][j] = o.chessBoard[i][j];
+            o.chessBoard[i][j] = nullptr;
+        }
+    }
 
+    for(auto &i: o.black){      //copy and clear
+        black.push_back(i);
+    }
+    o.black.clear();
+
+    for(auto &i: o.white){
+        white.push_back(i);
+    }
+    o.white.clear();
 }
 
-ChessBoard ChessBoard::operator=(const ChessBoard&){
+ChessBoard& ChessBoard::operator=(const ChessBoard& o){
+    
+    if(this == &o)
+        return *this;
+    
+    for(int i=0; i<SIZE; i++){
+        for(int j=0; j<SIZE; j++){
+            shared_ptr<ChessPiece> toAdd(copyPiece(o.chessBoard[i][j]));
+            chessBoard[i][j] = toAdd;
+        }
+    }
+    for(const auto &i: o.black){
+        black.push_back(i);
+    }
+    for(const auto &i: o.white){
+        white.push_back(i);
+    }
 
+    return *this;
 }
 
 bool ChessBoard::isCheck() const{
@@ -103,29 +161,30 @@ bool ChessBoard::isStaleMate() const{
 
 }
 
-//returns possible movements from a specific chesspiece. the returned set is empty if there isn't any piece or if there aren't any possible moves
-set<std::pair<int, int>> ChessBoard::getPossiblemovements(int _row, int _col) const{
-    
-}
-
-//returns possible movements for a specific chesspiece in the position of the list of pieces chosen by side
-set<std::pair<int, int>> ChessBoard::getPossiblemovementsByIndex(int index, side c) const{
-
-}
-
-//returns the type of move that was done
-move ChessBoard::move(pair<int, int>, pair<int, int>) const{
-
-}
 
 //returns a string containing the board disposition
 std::string ChessBoard::notToString() const{
-    
+    std::string res;
+
+    for(int i=0; i<SIZE; i++){
+        res += std::to_string(SIZE-i) + " ";
+        for(int j=0; j<SIZE; j++){
+            if(chessBoard[i][j]->getSide() == side::black)
+                res += static_cast<char>(chessBoard[i][j]->getRole());
+            else
+                res += std::tolower(static_cast<char>(chessBoard[i][j]->getRole()));
+        }
+        res += "\n";
+    }
+    for(int i=0; i<SIZE; i++){
+        res += 'A'+i;
+    }
+    res += "\n";
 }
 
 //returns the number of piece on the chessboard
 int ChessBoard::nOfPieces(side c) const{
-
+    return white.size() + black.size();
 }
 
 //does a promotion
--- ChessBoard.h
+++ ChessBoard.h
@@ -12,62 +12,63 @@
 #include "Pawn.h"
 #include "Dummy.h"
 
-
 #include <vector>
 #include <list>
 #include <memory>
 #include <utility>
 #include <string>
 
-using std::vector;
 using std::list;
-using std::shared_ptr;
 using std::pair;
-
+using std::shared_ptr;
+using std::vector;
 
 constexpr int SIZE = 8;
 
 constexpr char initial_roles[8][8] = {
 
-    {'T','C','A','D','R','A','C','T'},
-    {'P','P','P','P','P','P','P','P'},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {'P','P','P','P','P','P','P','P'},
-    {'T','C','A','D','R','A','C','T'}
+    {'T', 'C', 'A', 'D', 'R', 'A', 'C', 'T'},
+    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+    {'T', 'C', 'A', 'D', 'R', 'A', 'C', 'T'}};
+
+constexpr bool initial_colors[8][8] = {
+    //dummy chesspieces are black by convenction
+
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
 };
-
-
-constexpr bool initial_colors[8][8] = {             //dummy chesspieces are black by convenction
-
-    {0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0},
-    {1,1,1,1,1,1,1,1},
-    {1,1,1,1,1,1,1,1},
-};
-
-
 
 //add inclusion of other chessPieces
-class ChessBoard{
+class ChessBoard
+{
 
 private:
     shared_ptr<ChessPiece> chessBoard[SIZE][SIZE];
-    list<shared_ptr<ChessPiece>> black, white;//le liste e la matrice devono puntare agli stessi oggetti, questa è la soluzione meno peggiore che mi è venuta in mente ma si può fare di meglio
-    Pawn p{-1, -1, side::black};//pawn to promove
+    list<shared_ptr<ChessPiece>> black, white; //le liste e la matrice devono puntare agli stessi oggetti, questa è la soluzione meno peggiore che mi è venuta in mente ma si può fare di meglio
+    Pawn p{-1, -1, side::black};               //pawn to promove
+
+    void addToPieceList(const shared_ptr<ChessPiece>, const side);
+
+    shared_ptr<ChessPiece> copyPiece(const shared_ptr<ChessPiece>) const;
+
 
 public:
     ChessBoard();
-    ChessBoard(const ChessBoard&);      //copy
-    ChessBoard(const ChessBoard&&);     //move
+    ChessBoard(const ChessBoard &);  //copy
+    ChessBoard(ChessBoard &&); //move
 
-    ChessBoard operator=(const ChessBoard&);    //does a copy
+    ChessBoard& operator=(const ChessBoard &); //does a copy
 
     bool isCheck() const;
     bool isCheckMate() const;
@@ -75,19 +76,19 @@ public:
 
     //returns possible movements from a specific chesspiece. the returned set is empty if there isn't any piece or if there aren't any possible moves
     set<std::pair<int, int>> getPossiblemovements(int _row, int _col) const;
-    
+
     //returns possible movements for a specific chesspiece in the position of the list of pieces chosen by side
     set<std::pair<int, int>> getPossiblemovementsByIndex(int index, side c) const;
-    
+
     //returns the type of move that was done
     move move(pair<int, int>, pair<int, int>) const;
-    
+
     //returns a string containing the board disposition
     std::string notToString() const;
-    
+
     //returns the number of piece on the chessboard
     int nOfPieces(side c) const;
-    
+
     //does a promotion
     void promotion(role r);
 };
